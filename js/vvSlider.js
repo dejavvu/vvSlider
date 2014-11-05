65fbad1af9474a9cda41045c92881f235ab85186
N. Emre Alsancak <emre@dejavvu.com>
2014-11-05 16:04:14 +0200

    Committer: N. Emre Alsancak <emre@dejavvu.com> Initial commit Changes to be committed: 	new file:   css/vvSlider.css 	new file:   index.html 	new file:   js/vvSlider.js

---
+++ js/vvSlider.js
@@ -0,0 +1,16 @@
+/*
+    
+    Developer: N. Emre Alsancak
+    Email:     emre@dejavvu.com
+    Web:       http://www.dejavvu.com
+
+    ----------------------------------
+
+    Dejavvu Slider Module
+
+    Main Script File(vvSlider.js)
+
+    Version 0.1
+    11/04/2014
+    
+*/
